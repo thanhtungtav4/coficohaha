40f4c93083f077c196fa9e82c4bdfa4516511ec3
nguyenthanhtung74119@gmail.com <1406199774119>
2019-08-29 17:29:58 +0700

    truoc song lon - ADD LAYOUT

--- wp-content/themes/coficodev/page.php
+++ wp-content/themes/coficodev/page.php
@@ -2915,17 +2915,100 @@
                             <div class="item1">
                                 <div class="col-md-4 list-project-item">
                                     <div class="card mb-4">
-                                        <img class="card-img-top" src="<?php echo get_option('siteurl') ?>/wp-content/uploads/2014/10/vistaverde-thumbnail1.jpg">
+                                        <img class="card-img-top" src="<?php echo get_option('siteurl') ?>/wp-content/uploads/2017/11/Casino-2.jpg">
                                         <div class="card-body content-box-card">
-                                            <p class="h4-titile-project"><a href="<?php echo get_option('siteurl') ?>/project/vista-verde-residental/">VISTA VERDE</a></p>
+                                            <p class="h4-titile-project"><a href="<?php echo get_option('siteurl') ?>/project/du-an-condotel-khu-phuc-hop-nam-hoi-an/">Khu phức hợp Nam Hội An</a></p>
                                             <p class="project-home-content">
-                                                <strong>Dự Án:</strong> Vista Verde
+                                                <strong>Dự Án:</strong>Condotel – Khu phức hợp Nam Hội An
                                             </p>
                                             <p class="project-home-content">
-                                                <strong>Địa điểm:</strong> Q.2, TP.HCM
+                                                <strong>Địa điểm:</strong> Hội An, Quảng Nam
                                             </p>
                                             <p class="project-home-content">
-                                                <a href="<?php echo get_option('siteurl') ?>/project/vista-verde-residental/" class="more_duan">+ Xem Thêm...</a>
+                                                <a href="<?php echo get_option('siteurl') ?>/project/du-an-condotel-khu-phuc-hop-nam-hoi-an/" class="more_duan">+ Xem Thêm...</a>
+                                            </p>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item1">
+                                <div class="col-md-4 list-project-item">
+                                    <div class="card mb-4">
+                                        <img class="card-img-top" src="<?php echo get_option('siteurl') ?>/wp-content/uploads/2019/08/Hoya_1-1-700x406.jpg">
+                                        <div class="card-body content-box-card">
+                                            <p class="h4-titile-project"><a href="<?php echo get_option('siteurl') ?>/project/nha-may-hoya-lens/">Hoya Lens</a></p>
+                                            <p class="project-home-content">
+                                                <strong>Dự Án:</strong>  Nhà Máy Hoya Lens
+                                            </p>
+                                            <p class="project-home-content">
+                                                <strong>Địa điểm:</strong> Quảng Ngãi
+                                            </p>
+                                            <p class="project-home-content">
+                                                <a href="<?php echo get_option('siteurl') ?>/project/nha-may-hoya-lens/" class="more_duan">+ + Xem Thêm...</a>
+                                            </p>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- -->
+                            <div class="item1">
+                                <div class="col-md-4 list-project-item">
+                                    <div class="card mb-4">
+                                        <img class="card-img-top" src="<?php echo get_option('siteurl') ?>/wp-content/uploads/2019/08/56157726_1174695192689777_8907272635826569216_o.jpg">
+                                        <div class="card-body content-box-card">
+                                            <p class="h4-titile-project"><a href="<?php echo get_option('siteurl') ?>/project/sieu-thi-go/">SIÊU THỊ GO!</a></p>
+                                            <p class="project-home-content">
+                                                <strong>Dự Án:</strong>  SIÊU THỊ GO! QUẢNG NGÃI
+                                            </p>
+                                            <p class="project-home-content">
+                                                <strong>Địa điểm:</strong> Quảng Ngãi
+                                            </p>
+                                            <p class="project-home-content">
+                                                <a href="<?php echo get_option('siteurl') ?>/project/sieu-thi-go/" class="more_duan">+ Xem Thêm...</a>
+                                            </p>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item1">
+                                <div class="col-md-4 list-project-item">
+                                    <div class="card mb-4">
+                                        <img class="card-img-top" src="<?php echo get_option('siteurl') ?>/wp-content/uploads/2019/08/TIR-Hinh-anh-du-an-1.jpg">
+                                        <div class="card-body content-box-card">
+                                            <p class="h4-titile-project"><a href="<?php echo get_option('siteurl') ?>/project/tirol-choco-vietnam-factory/">
+                                                    Tirol Choco Việt Nam</a></p>
+                                            <p class="project-home-content">
+                                                <strong>Dự Án:</strong>  Nhà máy Tirol Choco Việt Nam
+                                            </p>
+                                            <p class="project-home-content">
+                                                <strong>Địa điểm:</strong>  Đồng Nai, Việt Nam
+                                            </p>
+                                            <p class="project-home-content">
+                                                <a href="<?php echo get_option('siteurl') ?>/project/tirol-choco-vietnam-factory/" class="more_duan">+ Xem Thêm...</a>
+                                            </p>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Hausneo Apartment ESTELLA HEIGHTS 2 -->
+                            <div class="item1">
+                                <div class="col-md-4 list-project-item">
+                                    <div class="card mb-4">
+                                        <img class="card-img-top" src="<?php echo get_option('siteurl') ?>/wp-content/uploads/2014/10/hausneo_031.jpg">
+                                        <div class="card-body content-box-card">
+                                            <p class="h4-titile-project"><a href="<?php echo get_option('siteurl') ?>/project/du-an-can-ho-hausneo/">CĂN HỘ HAUSNEO</a></p>
+                                            <p class="project-home-content">
+                                                <strong>Dự Án:</strong> Căn Hộ Hausneo
+                                            </p>
+                                            <p class="project-home-content">
+                                                <strong>Địa điểm:</strong> Q.9, TP.HCM
+                                            </p>
+                                            <p class="project-home-content">
+                                                <a href="<?php echo get_option('siteurl') ?>/project/du-an-can-ho-hausneo/" class="more_duan">+ Xem Thêm...</a>
                                             </p>
 
                                         </div>
@@ -2952,20 +3035,21 @@
                                     </div>
                                 </div>
                             </div>
+                            <!-- Hoya Lens-->
                             <div class="item1">
                                 <div class="col-md-4 list-project-item">
                                     <div class="card mb-4">
-                                        <img class="card-img-top" src="<?php echo get_option('siteurl') ?>/wp-content/uploads/2014/10/hausneo_031.jpg">
+                                        <img class="card-img-top" src="<?php echo get_option('siteurl') ?>/wp-content/uploads/2015/12/Sadora03-700x394.jpg">
                                         <div class="card-body content-box-card">
-                                            <p class="h4-titile-project"><a href="<?php echo get_option('siteurl') ?>/project/du-an-can-ho-hausneo/">CĂN HỘ HAUSNEO</a></p>
+                                            <p class="h4-titile-project"><a href="<?php echo get_option('siteurl') ?>/project/sadora-condominium/">Căn Hộ Sadora</a></p>
                                             <p class="project-home-content">
-                                                <strong>Dự Án:</strong>  Căn Hộ Hausneo
+                                                <strong>Dự Án:</strong>  Căn Hộ Sadora
                                             </p>
                                             <p class="project-home-content">
-                                                <strong>Địa điểm:</strong> Q.9, TP.HCM
+                                                <strong>Địa điểm:</strong> Thủ Thiêm, Quận 2, TP. Hồ Chí Minh
                                             </p>
                                             <p class="project-home-content">
-                                                <a href="<?php echo get_option('siteurl') ?>/project/du-an-can-ho-hausneo/" class="more_duan">+ Xem Thêm...</a>
+                                                <a href="<?php echo get_option('siteurl') ?>/project/sadora-condominium/" class="more_duan">+ Xem Thêm...</a>
                                             </p>
 
                                         </div>
@@ -2975,17 +3059,18 @@
                             <div class="item1">
                                 <div class="col-md-4 list-project-item">
                                     <div class="card mb-4">
-                                        <img class="card-img-top" src="<?php echo get_option('siteurl') ?>/wp-content/uploads/2014/10/gateway-thao-dien.jpg">
+                                        <img class="card-img-top" src="<?php echo get_option('siteurl') ?>/wp-content/uploads/2014/10/vistaverde.jpg">
                                         <div class="card-body content-box-card">
-                                            <p class="h4-titile-project"><a href="<?php echo get_option('siteurl') ?>/project/can-ho-gateway-thao-dien/">GATEWAY THẢO ĐIỀN</a></p>
+                                            <p class="h4-titile-project"><a href="<?php echo get_option('siteurl') ?>/project/vista-verde-residental/">
+                                                    VISTA VERDE</a></p>
                                             <p class="project-home-content">
-                                                <strong>Dự Án:</strong>  GATEWAY THẢO ĐIỀN
+                                                <strong>Dự Án:</strong> Căn hộ Vista Verde
                                             </p>
                                             <p class="project-home-content">
-                                                <strong>Địa điểm:</strong> Q.2, TP.HCM
+                                                <strong>Địa điểm:</strong> Quận 2, TP. Hồ Chí Minh
                                             </p>
                                             <p class="project-home-content">
-                                                <a href="<?php echo get_option('siteurl') ?>/project/can-ho-gateway-thao-dien/" class="more_duan">+ Xem Thêm...</a>
+                                                <a href="<?php echo get_option('siteurl') ?>/project/vista-verde-residental/" class="more_duan">+ Xem Thêm...</a>
                                             </p>
 
                                         </div>
