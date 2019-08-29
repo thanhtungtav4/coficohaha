b327abe240125e393468558f2c78eb4acd1fa2d2
nguyenthanhtung74119@gmail.com <1406199774119>
2019-08-29 09:13:53 +0700

    truoc song lon - ADD LAYOUT

--- wp-content/themes/coficodev/page.php
+++ wp-content/themes/coficodev/page.php
@@ -3086,6 +3086,9 @@
                             <a href="https://www.cofico.com.vn/tam-nhin-su-menh-gia-tri/" target="_self">
                                 <img width="237" height="237" src="wp-content/uploads/2014/10/CORE-VALUES_FINISHED11.png" class="card-img" alt="CORE-VALUES_FINISHED1"></a>
                         </div>
+                        <div>
+                            <div class="text_sub_video"><span>THƯƠNG HIỆU UY TÍN VỀ AN TOÀN, THÂN THIỆN MÔI TRƯỜNG</span></div>
+                        </div>
                         <div class="img_cofico">
                             <a href="https://www.cofico.com.vn/tam-nhin-su-menh-gia-tri/" target="_self">
                                 <img width="237" height="237" src="wp-content/uploads/ZeroHarm.jpg" class="card-img" alt="ZeroHarm"></a>
