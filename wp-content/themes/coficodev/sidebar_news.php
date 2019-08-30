5cd88fe55f75107912aef0da35892de773ed1cdc
nguyenthanhtung74119@gmail.com <1406199774119>
2019-08-30 14:22:15 +0700

    truoc song lon - ADD LAYOUT

--- wp-content/themes/coficodev/sidebar_news.php
+++ wp-content/themes/coficodev/sidebar_news.php
@@ -1,95 +1,42 @@
 <?php if(ICL_LANGUAGE_CODE=='en'): ?>
     <div class="col-sm-4 padding30">
-        <div class="video_ifm">
-            <div class="text_sub_video"><span>VIDEO NEWS</span></div>
-            <div class="video-responsive">
-                <iframe width="350" height="220" src="https://www.youtube.com/embed/6NOw18qZFkQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div></div>
-        <div class="hr-thin">
-            <hr>
+        <div>
+            <div class="text_sub_video"><span>COFICO’S CORE VALUES</span></div>
         </div>
-        <div class="user_pro">
-            <div class="text_sub_video"><span>INTERNATIONAL STRATEGIC SHAREHOLDERS</span></div>
-            <br>
-            <div class="row">
-                <div class="col">
-                    <div class="wpb_wrapper sub_news_cc">
-                        <div class="gap" style="line-height: 15px; height: 15px;"></div>
-                        <p><a style="color: #0072bc;" href="https://www.cofico.com.vn/thu-ngo/">Mr. Le Dang Xu</a></p>
-                        <p><a class="left_sub_b" href="https://www.cofico.com.vn/thu-ngo/">Chairman of BOD</a></p>
-                        <p><a class="left_sub_b" href="https://www.cofico.com.vn/thu-ngo/">Executive Chairman</a></p>
-                        <p><a class="left_sub_b" href="https://www.cofico.com.vn/thu-ngo/">COFICO</a></p>
-                        <p></p></div></div>
-                <div class="col"><img class="card-img" src="https://www.cofico.com.vn/wp-content/uploads/2014/10/Mr-Xu-giamdoc-150x150.jpg"></div>
-                <p class="sub_user"><em>“COFICO will surely gradually achieve the standards of an international contractor and simultaneously possess the advantage of understanding the specific requirements of the local market. From there, we will optimize the gained values to become a desirable workplace, a trusted partner, a qualified and reputable contractor, and a legal entity with high social responsibility. Combining these factors, COFICO will surely become the most respected construction company in Vietnam.”</em></p>
-                <p></p></div>
-            <div class="hr-thin">
-                <hr>
-            </div>
-sub
-            <div>
-                <div class="text_sub_video"><span>COFICO’S CORE VALUES</span></div></div>
 
-            <div class="img_cofico">
-                <a href="https://www.cofico.com.vn/tam-nhin-su-menh-gia-tri/" target="_self" rel="noopener noreferrer"><br>
-                    <img width="237" height="237" src="https://www.cofico.com.vn/wp-content/uploads/2014/10/CORE-VALUES_FINISHED11.png" class="card-img" alt="CORE-VALUES_FINISHED1"></a>
-            </div>
-            <div>
-                <div class="text_sub_video"><span>
-                    REPUTABLE BRAND FOR HEALTH, SAFETY & ENVIRONMENT</span></div>
-            </div>
-            <div class="img_cofico">
-                <img src="https://www.cofico.com.vn/wp-content/uploads/ZeroHarm.jpg" class="card-img" alt="CORE-VALUES_FINISHED1">
-            </div>
-            <div>
-                <div class="text_sub_video"><span>MODERN TECHNOLOGY</span></div>
-            </div>
-            <div class="img_cofico">
-                <img width="350" height="250" src="https://www.cofico.com.vn/wp-content/uploads/tech.jpg" class="card-img" alt="CORE-VALUES_FINISHED1">
+        <div class="img_cofico">
+            <a href="https://www.cofico.com.vn/tam-nhin-su-menh-gia-tri/" target="_self" rel="noopener noreferrer">
+                <br>
+                <img width="237" height="237" src="https://www.cofico.com.vn/wp-content/uploads/2014/10/CORE-VALUES_FINISHED11.png" class="card-img" alt="CORE-VALUES_FINISHED1"></a>
+        </div>
+        <div>
+            <div class="text_sub_video">
+                <span>REPUTATION FOR HEALTH, SAFETY & ENVIRONMENT</span>
             </div>
         </div>
+        <div class="img_cofico">
+            <img src="https://www.cofico.com.vn/wp-content/uploads/ZeroHarm.jpg" class="card-img" alt="CORE-VALUES_FINISHED1">
+        </div>
+        <div>
+            <div class="text_sub_video">
+                <span>MODERN TECHNOLOGY</span>
+            </div>
+        </div>
+        <div class="img_cofico">
+            <img src="https://www.cofico.com.vn/wp-content/uploads/tech.jpg" class="card-img" alt="CORE-VALUES_FINISHED1">
+        </div>
+    </div>
     </div>
 <?php elseif(ICL_LANGUAGE_CODE=='vi'): ?>
     <div class="col-sm-4 padding30">
-        <div class="video_ifm">
-            <div class="text_sub_video"><span>VIDEO NEWS</span></div>
-            <div class="video-responsive">
-                <iframe width="350" height="220" src="https://www.youtube.com/embed/6NOw18qZFkQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-           </div>
-        </div>
-        <div class="hr-thin">
-            <hr>
-        </div>
         <div class="user_pro">
-            <div class="text_sub_video"><span>CỔ ĐÔNG CHIẾN LƯỢC</span></div>
-            <br>
-            <div class="row">
-                <div class="col">
-                    <div class="wpb_wrapper sub_news_cc">
-                        <div class="gap" style="line-height: 15px; height: 15px;"></div>
-                        <p><a style="color: #0072bc;" href="https://www.cofico.com.vn/thu-ngo/">Lê Đăng Xu</a></p>
-                        <p><a class="left_sub_b" href="https://www.cofico.com.vn/thu-ngo/">Chủ tịch HĐQT</a></p>
-                        <p><a class="left_sub_b" href="https://www.cofico.com.vn/thu-ngo/">Tổng Giám Đốc </a></p>
-                        <p><a class="left_sub_b" href="https://www.cofico.com.vn/thu-ngo/">COFICO</a></p>
-                        <p></p>
-                    </div>
-                </div>
-                <div class="col">
-                    <img src="https://www.cofico.com.vn/wp-content/uploads/2014/10/Mr-Xu-giamdoc-150x150.jpg" class="card-img">
-                </div>
-                <p class="sub_user"><em>“COFICO chắc chắn dần dần sẽ đạt được các chuẩn mực của một nhà thầu quốc tế, đồng thời sở hữu lợi thế thấu hiểu các yêu cầu đặc thù của thị trường địa phương. Từ đó chúng ta sẽ tối ưu hóa các giá trị mang lại để trở thành một nơi làm việc mong ước, một đối tác tin cậy, một nhà thầu chất lượng, uy tín, và là một pháp nhân có trách nhiệm cao với cộng đồng. Tổng hòa các yếu tố đó, COFICO chắc chắn sẽ trở thành một công ty xây dựng được tôn trọng nhất tại Việt Nam.”</em></p>
-                <p></p>
-            </div>
-            <div class="hr-thin">
-                <hr>
-            </div>
             <div>
                 <div class="text_sub_video"><span>GIÁ TRỊ CỐT LÕI CỦA COFICO</span></div>
             </div>
 
-
             <div class="img_cofico">
-                <a href="https://www.cofico.com.vn/tam-nhin-su-menh-gia-tri/" target="_self" rel="noopener noreferrer"><br>
+                <a href="https://www.cofico.com.vn/tam-nhin-su-menh-gia-tri/" target="_self" rel="noopener noreferrer">
+                    <br>
                     <img width="237" height="237" src="https://www.cofico.com.vn/wp-content/uploads/2014/10/CORE-VALUES_FINISHED11.png" class="card-img" alt="CORE-VALUES_FINISHED1"></a>
             </div>
             <div>
