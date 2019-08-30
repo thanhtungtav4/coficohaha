b50103a8dda2c14bdcab7e467d68b77ea0dde0dc
nguyenthanhtung74119@gmail.com <1406199774119>
2019-08-30 10:04:56 +0700

    truoc song lon - ADD LAYOUT

--- wp-content/themes/coficodev/footer.php
+++ wp-content/themes/coficodev/footer.php
@@ -4,7 +4,7 @@
 <?php  require get_template_directory() . '/list-du-an-mobile.php';?>
 </div>
 <!-- footer is show all page -->
-<section class="win">
+<section class="win" style="display: none;">
     <div class="container-fluid">
         <h2 class="title_home_mt">  <?php if(ICL_LANGUAGE_CODE=='en'): ?> CLIENTS & PARTNERS  <?php elseif(ICL_LANGUAGE_CODE=='vi'): ?> GIẢI THƯỞNG & CHỨNG NHẬN   <?php endif; ?> </h2>
         <div class="border_title"></div>
