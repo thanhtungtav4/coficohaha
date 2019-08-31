b266cbb54e787ac2a0f870ecb9f00cfc6fbe8f74
nguyenthanhtung74119@gmail.com <1406199774119>
2019-08-31 10:05:22 +0700

    tung is 2-9

--- wp-content/themes/coficodev/footer.php
+++ wp-content/themes/coficodev/footer.php
@@ -6,7 +6,7 @@
 <!-- footer is show all page -->
 <section class="win">
     <div class="container-fluid">
-        <h2 class="title_home_mt mt-list-active-sider">  <?php if(ICL_LANGUAGE_CODE=='en'): ?> CLIENTS & PARTNERS  <?php elseif(ICL_LANGUAGE_CODE=='vi'): ?> GIẢI THƯỞNG & CHỨNG NHẬN   <?php endif; ?> </h2>
+        <h2 class="title_home_mt mt-list-active-sider">  <?php if(ICL_LANGUAGE_CODE=='en'): ?> CLIENTS & PARTNERS  <?php elseif(ICL_LANGUAGE_CODE=='vi'): ?>  ĐỐI TÁC & KHÁCH HÀNG <?php endif; ?> </h2>
         <div class="border_title"></div>
         <div class="row">
             <div class="owl-carousel owl-theme owl-loaded owl-drag">
