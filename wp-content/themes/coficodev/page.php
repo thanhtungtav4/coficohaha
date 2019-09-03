ea14679e2a6850d81e26acd7d07eaf87f562a9f0
nguyenthanhtung74119@gmail.com <1406199774119>
2019-09-03 09:29:19 +0700

    change speed banner

--- wp-content/themes/coficodev/page.php
+++ wp-content/themes/coficodev/page.php
@@ -879,8 +879,8 @@
                                         <img src="<?php echo get_option('siteurl') ?>/wp-content/uploads/2014/10/Gammonlogo2.jpg">
                                     </a>
                                     <br>
-                                   <p> <a style="color: #0072bc;" href="https://www.gammonconstruction.com/en/html/front/index.html"><strong>Gammon Construction
-                                           </strong></a></p>
+                                   <a style="color: #0072bc;" href="https://www.gammonconstruction.com/en/html/front/index.html"><strong>Gammon Construction
+                                           </strong></a>
                                     <br>
                                     <a style="color: #0072bc;" href="https://www.gammonconstruction.com/en/html/front/index.html">Website</a>
 
