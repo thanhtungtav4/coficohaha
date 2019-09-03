f0ac21540a71df4cb8b2552cdab7bddf36ec9837
nguyenthanhtung74119@gmail.com <1406199774119>
2019-09-03 16:43:30 +0700

    change speed banner

--- wp-content/themes/coficodev/page-lien-he.php
+++ wp-content/themes/coficodev/page-lien-he.php
@@ -72,7 +72,7 @@
         </div>
     </nav>
 </header>
-<section>
+<section style="margin-top: 10vh;">
     <div class="row">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.511182989742!2d106.67807621411639!3d10.772105462220672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f422148b433%3A0x6cfa75d049afa95!2zQ8O0bmcgVHkgQ-G7lSBQaOG6p24gWMOieSBE4buxbmcgU-G7kSAx!5e0!3m2!1sen!2s!4v1561721281754!5m2!1sen!2s" width="100%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
